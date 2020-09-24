2a9d8512f7e2213fe7475b072fbf5c9b6331814e
nad7wf <nad7wf@umsystem.edu>
2020-09-24 15:26:03 -0500

    updated RF function to output a list of two data.frames: preds and cc.
    Added Readme

--- R/RF.R
+++ R/RF.R
@@ -1,46 +1,91 @@
-RF <- function (dat, cv_method) {
+#' CV-optimized Random Forest
+#'
+#' In plant breeding, it is common to leave one population out when training a machine learning model. This package
+#' allows the user to run random forest using one population as the test set, as well as with other common CV
+#' strategies (e.g. 5-fold, 10-fold, etc.).
+#'
+#' @param data a data.frame with columns "Y", "GE_ID", "Parent_A", "Parent_B", followed by one column per genetic marker.
+#' @param cv_method Either an integer, specifying the number of folds, or "family" to fold according to population.
+#'
+#' @return A list of two data.frames, one containing the predictions for each GE, and one containing the correlation coefficients
+#' between observed and predicted values for each fold.
+#'
+#' @importFrom magrittr %>%
+#'
+#' @export
+#'
+#' @examples
+#' set.seed(76123)
+#' row_num <- 1000
+#'
+#' ex <- data.frame(
+#'     Y = runif(row_num, 30, 150),
+#'     GE_ID = runif(row_num, 1e8, 2e8),
+#'     Parent_A = sample(LETTERS[1:13], row_num, replace = TRUE),
+#'     Parent_B = sample(LETTERS[14:26], row_num, replace = TRUE)
+#' ) %>%
+#'     cbind(replicate(10, runif(row_num, 0, 1)) %>%
+#'               as.data.frame() %>%
+#'               magrittr::set_names(paste0("Marker", seq(10))))
+#'
+#' result <- RF(ex, "family")
+RF <- function (data, cv_method) {
 
-    `%>%` <- magrittr::`%>%`
-
-    # Shuffle data.frame.
+    # Create an index based on CV method for subsetting train/test.
     set.seed(32256)
-    row_order <- sample(nrow(dat))
-    dat <- data.frame(dat)[row_order, ]
 
-    # Create index column based on CV method for subsetting train/test.
-    if (cv_method == 1) {
-        dat$idx <- seq(nrow(dat))
-    } else if (is.numeric(cv_method)) {
-        dat$idx <- cut(seq(nrow(dat)),
-                       breaks = cv_method,
-                       labels = FALSE)
+    if (is.numeric(cv_method)) {
+        idx <- seq(nrow(dat)) %>%
+            cut(breaks = cv_method,
+                labels = FALSE) %>%
+            sample()
     } else if (cv_method %in% c("Family", "family")) {
-        dat$idx <- paste0(dat$A, dat$B) %>%
-            factor() %>%
-            as.numeric()
+        idx <- paste0(dat$Parent_A, dat$Parent_B)
+    }
+
+    comb <- function(x, ...) {
+        lapply(seq_along(x),
+               function(i) c(x[[i]],
+                             lapply(list(...),
+                                    function(y) y[[i]])))
     }
 
     # Establish parallel enviroment to run CV folds in tandem.
-    cl <- parallel::makeCluster(parallel::detectCores() - 2)
+    cl <- parallel::makeCluster(2)
     doParallel::registerDoParallel(cl)
 
-    # Run random forest algorithm on different folds in random order.
-    set.seed(66824)
-    res <- foreach (test_idx = sample(unique(dat$idx))) %dopar% {
+    # Run random forest algorithm in random order on different folds.
+    res <- foreach::foreach (test_idx = sample(unique(idx)),
+                             .combine = "comb",
+                             .multicombine = TRUE,
+                             .init = list(list(), list())) %dopar% {
 
-        rf_mod <- randomForest::randomForest(formula = Yield ~ .,
-                                             data = dat[!dat$idx %in% test_idx, c(1, 6:ncol(dat))],
-                                             importance = TRUE)
+                                 train <- dat[idx != test_idx, c(1, 5:ncol(dat))]
+                                 test <- dat[idx == test_idx, c(1, 5:ncol(dat))]
 
-        stats::predict(rf_mod, dat[dat$idx %in% test_idx, c(1, 6:ncol(dat))]) %>%
-            data.frame(GE_ID = dat$GE_ID[dat$idx == test_idx],
-                       Pop = paste0(dat$A[dat$idx == test_idx],
-                                    dat$B[dat$idx == test_idx]),
-                       Pred = .)
+                                 rf_mod <- randomForest::randomForest(formula = Y ~ .,
+                                                                      data = train)
 
-    }
+                                 preds <- stats::predict(rf_mod, test) %>%
+                                     data.frame(
+                                         GE_ID = dat$GE_ID[idx == test_idx],
+                                         Pop = paste0(dat$Parent_A[idx == test_idx],
+                                                      dat$Parent_B[idx == test_idx]),
+                                         Pred = .
+                                     )
+
+                                 cc <- stats::cor(data.frame(preds$Pred, dat$Y[idx == test_idx]))[[2]] %>%
+                                     data.frame(Fold = test_idx,
+                                                Cor = .)
+
+                                 return(list(preds, cc))
+                             } %>%
+        lapply(function (list_ele) {
+            do.call("rbind", list_ele)
+        }) %>%
+        `names<-`(c("preds", "cc"))
 
     parallel::stopCluster(cl)
-    return(rbindlist(res))
+    return(res)
 
 }
