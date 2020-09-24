84ad16ff6589b984e918f914446319772900cc37
nad7wf <nad7wf@umsystem.edu>
2020-09-24 15:29:14 -0500

    Updated Readme

--- R/RF.R
+++ R/RF.R
@@ -4,32 +4,35 @@
 #' allows the user to run random forest using one population as the test set, as well as with other common CV
 #' strategies (e.g. 5-fold, 10-fold, etc.).
 #'
-#' @param data a data.frame with columns "Y", "GE_ID", "Parent_A", "Parent_B", followed by one column per genetic marker.
+#' @param dat a data.frame with columns "Y", "GE_ID", "Parent_A", "Parent_B", followed by one column per genetic marker.
 #' @param cv_method Either an integer, specifying the number of folds, or "family" to fold according to population.
 #'
 #' @return A list of two data.frames, one containing the predictions for each GE, and one containing the correlation coefficients
 #' between observed and predicted values for each fold.
 #'
 #' @importFrom magrittr %>%
+#' @importFrom foreach %dopar%
 #'
 #' @export
 #'
 #' @examples
+#' library(magrittr)
+#'
 #' set.seed(76123)
 #' row_num <- 1000
 #'
 #' ex <- data.frame(
 #'     Y = runif(row_num, 30, 150),
 #'     GE_ID = runif(row_num, 1e8, 2e8),
-#'     Parent_A = sample(LETTERS[1:13], row_num, replace = TRUE),
-#'     Parent_B = sample(LETTERS[14:26], row_num, replace = TRUE)
+#'     Parent_A = sample(LETTERS[1:4], row_num, replace = TRUE),
+#'     Parent_B = sample(LETTERS[5:8], row_num, replace = TRUE)
 #' ) %>%
 #'     cbind(replicate(10, runif(row_num, 0, 1)) %>%
 #'               as.data.frame() %>%
 #'               magrittr::set_names(paste0("Marker", seq(10))))
 #'
 #' result <- RF(ex, "family")
-RF <- function (data, cv_method) {
+RF <- function (dat, cv_method) {
 
     # Create an index based on CV method for subsetting train/test.
     set.seed(32256)
@@ -50,14 +53,27 @@ RF <- function (data, cv_method) {
                                     function(y) y[[i]])))
     }
 
+    # R package will not pass check if using more than 2 cores.
+    # Only use 2 cores during check, otherwise use all but 1.
+    chk <- Sys.getenv("_R_CHECK_LIMIT_CORES_", "")
+    if (nzchar(chk) && chk == "TRUE") {
+        # use 2 cores in CRAN/Travis/AppVeyor
+        num_workers <- 2L
+    } else {
+        # use all cores in devtools::test()
+        num_workers <- (parallel::detectCores() - 1)
+    }
+
     # Establish parallel enviroment to run CV folds in tandem.
-    cl <- parallel::makeCluster(2)
+    cl <- parallel::makeCluster(num_workers)
     doParallel::registerDoParallel(cl)
+    parallel::clusterExport(cl, "%>%")
 
     # Run random forest algorithm in random order on different folds.
     res <- foreach::foreach (test_idx = sample(unique(idx)),
                              .combine = "comb",
                              .multicombine = TRUE,
+                             .packages = "magrittr",
                              .init = list(list(), list())) %dopar% {
 
                                  train <- dat[idx != test_idx, c(1, 5:ncol(dat))]
